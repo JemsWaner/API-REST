39fd6775af896d66a04cfe57d74b9356d7a089db
JemsWaner <110148531+JemsWaner@users.noreply.github.com>
2025-07-07 23:42:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = 3000;
+const cors = require("cors"); 
 
 app.use(cors())
 app.use(express.json());
